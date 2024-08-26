d33699e1adb9f6f0702c0dda0f7e0d20c7bad712
spenning <whimizera@mailbox.org>
2024-08-26 16:31:55 +0200

    strlen converted

--- tests/libft_tests/ft_strlen_test.c
+++ tests/libft_tests/ft_strlen_test.c
@@ -6,7 +6,7 @@
 /*   By: mynodeus <mynodeus@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/17 06:27:38 by mynodeus      #+#    #+#                 */
-/*   Updated: 2024/08/26 14:53:12 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/26 16:31:14 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,13 @@ int	g_fail_strlen = 0;
 
 int	strlen_cmp(int test_count, char *test)
 {
-	FILE	*errorlog;
 	size_t	ft;
 	size_t	org;
 
-	errorlog = fopen("logs/error_log.txt", "a");
-	if (errorlog == NULL)
-	{
-		printf("Error opening log file\n");
-		return (1);
-	}
 	org = strlen(test);
 	ft = ft_strlen(test);
 	if (ft != org)
-		g_fail_strlen += ft_log_int(test_count, errorlog, org, ft);
+		g_fail_strlen += ft_log_int(test_count, org, ft);
 	else
 		printf(GRN "%d OK " RESET, test_count);
 	return (test_count + 1);
