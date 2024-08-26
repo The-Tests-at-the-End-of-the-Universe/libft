4c91ba905016415c98e4baca34b06a8072d4736f
spenning <whimizera@mailbox.org>
2024-08-26 16:35:48 +0200

    converted ft_substr

--- tests/libft_tests/ft_substr_test.c
+++ tests/libft_tests/ft_substr_test.c
@@ -6,7 +6,7 @@
 /*   By: mynodeus <mynodeus@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/17 06:51:07 by mynodeus      #+#    #+#                 */
-/*   Updated: 2024/08/26 14:53:32 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/26 16:35:27 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,11 @@ char	*g_result = NULL;
 
 int	substr_cmp(int test_count, char *test1, int start, size_t len)
 {
-	FILE	*errorlog;
 	char	*ft;
 
-	errorlog = fopen("logs/error_log.txt", "a");
-	if (errorlog == NULL)
-	{
-		printf("Error opening log file\n");
-		return (1);
-	}
 	ft = ft_substr(test1, start, len);
 	if (strcmp(ft, g_result))
-		g_fail_substr += ft_log_str(test_count, errorlog, g_result, ft);
+		g_fail_substr += ft_log_str(test_count, g_result, ft);
 	else
 		printf(GRN "%d OK " RESET, test_count);
 	free(ft);
