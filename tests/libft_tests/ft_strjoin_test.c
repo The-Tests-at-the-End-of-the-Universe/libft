f45e7d6fe5306b390876a8674f52fdb733308a2c
spenning <whimizera@mailbox.org>
2024-08-26 16:29:20 +0200

    strjoin converted

--- tests/libft_tests/ft_strjoin_test.c
+++ tests/libft_tests/ft_strjoin_test.c
@@ -6,7 +6,7 @@
 /*   By: mynodeus <mynodeus@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/17 05:57:44 by mynodeus      #+#    #+#                 */
-/*   Updated: 2024/08/26 14:52:59 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/26 16:29:05 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,11 @@ int	g_fail_strjoin = 0;
 
 int	strjoin_cmp(int test_count, char *test1, char *test2, char	*result)
 {
-	FILE	*errorlog;
 	char	*ft;
 
-	errorlog = fopen("logs/error_log.txt", "a");
-	if (errorlog == NULL)
-	{
-		printf("Error opening log file\n");
-		return (1);
-	}
 	ft = ft_strjoin(test1, test2);
 	if (strcmp(ft, result))
-		g_fail_strjoin += ft_log_str(test_count, errorlog, result, ft);
+		g_fail_strjoin += ft_log_str(test_count, result, ft);
 	else
 		printf(GRN "%d OK " RESET, test_count);
 	free(ft);
