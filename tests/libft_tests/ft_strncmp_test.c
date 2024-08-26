1feca1030dbefd275434400639d6718ab3955822
spenning <whimizera@mailbox.org>
2024-08-26 16:32:50 +0200

    converted strncmp

--- tests/libft_tests/ft_strncmp_test.c
+++ tests/libft_tests/ft_strncmp_test.c
@@ -6,7 +6,7 @@
 /*   By: mynodeus <mynodeus@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/17 06:27:38 by mynodeus      #+#    #+#                 */
-/*   Updated: 2024/08/26 14:53:18 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/26 16:32:38 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,13 @@ int	g_fail_strncmp = 0;
 
 int	strncmp_cmp(int test_count, char *test1, char *test2, size_t n)
 {
-	FILE	*errorlog;
 	size_t	ft;
 	size_t	org;
 
-	errorlog = fopen("logs/error_log.txt", "a");
-	if (errorlog == NULL)
-	{
-		printf("Error opening log file\n");
-		return (1);
-	}
 	org = strncmp(test1, test2, n);
 	ft = ft_strncmp(test1, test2, n);
 	if (ft != org)
-		g_fail_strncmp += ft_log_int(test_count, errorlog, org, ft);
+		g_fail_strncmp += ft_log_int(test_count, org, ft);
 	else
 		printf(GRN "%d OK " RESET, test_count);
 	return (test_count + 1);
