c001663c613663b1446bc2f2ba9ae2ef0abfa401
spenning <whimizera@mailbox.org>
2024-08-26 15:32:14 +0200

    rename libft to tests

---
+++ tests/libft_tests/ft_istotester.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_istotester.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spenning <spenning@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/05/16 13:29:29 by spenning      #+#    #+#                 */
+/*   Updated: 2024/08/26 15:30:49 by spenning      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft_tester.h"
+#include <ctype.h>
+
+int	g_fail_isto = 0;
+
+int	isto_cmp(int tc, int c, int (*f)(int), int (*ft)(int))
+{
+	int		org_func;
+	int		ft_func;
+
+	org_func = f(c);
+	ft_func = ft(c);
+	if (org_func != ft_func)
+	{
+		printf(RED "%d FAIL "RESET, tc);
+		dprintf(2, "[%d] \n", tc);
+		dprintf(2, "org function: %d\n", org_func);
+		dprintf(2, "lib function: %d\n", ft_func);
+		g_fail_isto += 1;
+	}
+	else
+		printf(GRN "%d OK " RESET, tc);
+	return (tc + 1);
+}
+
+int	isto_test(char *function_name, int (*f)(int), int (*ft)(int))
+{
+	int	test_count;
+
+	test_count = 1;
+	(void)function_name;
+	test_count = isto_cmp(test_count, 'a', f, ft);
+	test_count = isto_cmp(test_count, 'A', f, ft);
+	test_count = isto_cmp(test_count, 'z', f, ft);
+	test_count = isto_cmp(test_count, '\n', f, ft);
+	test_count = isto_cmp(test_count, '\200', f, ft);
+	test_count = isto_cmp(test_count, '\100', f, ft);
+	test_count = isto_cmp(test_count, 359, f, ft);
+	test_count = isto_cmp(test_count, 255, f, ft);
+	test_count = isto_cmp(test_count, '"', f, ft);
+	test_count = isto_cmp(test_count, ' ', f, ft);
+	test_count = isto_cmp(test_count, 1243, f, ft);
+	return (g_fail_isto);
+}
