ffffc3f061bf8bb288e1353650b7bab535e83750
spenning <whimizera@mailbox.org>
2024-08-26 15:59:53 +0200

    bzero converted

--- tests/libft_tests/ft_bzero_test.c
+++ tests/libft_tests/ft_bzero_test.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/16 12:48:45 by spenning      #+#    #+#                 */
-/*   Updated: 2024/08/26 14:51:51 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/26 15:53:47 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,10 @@ char	*init_test_alloc(char *test)
 	return (test_alloc);
 }
 
-int	bzero_cmp_exe(char	*ta, char	*td, int test_count, FILE *errorlog)
+int	bzero_cmp_exe(char	*ta, char	*td, int test_count)
 {
 	if (strcmp(ta, td))
-	{
-		fprintf(errorlog, "ft_bzero\n");
-		ft_log_str(test_count, errorlog, ta, td);
-		g_fail_bzero += 1;
-	}
+		g_fail_bzero += ft_log_str(test_count, ta, td);
 	else
 		printf(GRN "%d OK " RESET, test_count);
 	free(ta);
@@ -63,7 +59,6 @@ int	bzero_cmp_exe(char	*ta, char	*td, int test_count, FILE *errorlog)
 
 int	bzero_cmp(int test_count, char *test, size_t n)
 {
-	FILE	*errorlog;
 	char	*test_alloc;
 	char	*test_dup;
 
@@ -75,19 +70,9 @@ int	bzero_cmp(int test_count, char *test, size_t n)
 	test_dup = init_test_dup(test, test_alloc);
 	if (test_dup == NULL)
 		return (1);
-	errorlog = fopen("logs/error_log.txt", "a");
-	if (errorlog == NULL)
-	{
-		if (test_alloc)
-			free(test_alloc);
-		if (test_dup)
-			free(test_dup);
-		printf("Error opening log file\n");
-		return (1);
-	}
 	bzero(test_alloc, n);
 	ft_bzero(test_dup, n);
-	return (bzero_cmp_exe(test_alloc, test_dup, test_count, errorlog));
+	return (bzero_cmp_exe(test_alloc, test_dup, test_count));
 }
 
 int	bzero_test(void)
