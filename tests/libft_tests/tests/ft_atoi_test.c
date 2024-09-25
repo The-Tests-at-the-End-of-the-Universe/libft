0f4cfb6345772b3dcea2464eae84c84f0db07133
Sebastian Penning <whimizera@mailbox.org>
2024-09-25 15:04:02 +0200

    ft_atoi norm

--- tests/libft_tests/tests/ft_atoi_test.c
+++ tests/libft_tests/tests/ft_atoi_test.c
@@ -12,33 +12,32 @@
 
 #include <libft_tester.h>
 
-int	g_fail_atoi = 0;
+int							g_fail_atoi = 0;
 
-static const char * const tests[] = {
-	[ZERO] = "",
-	[ONE] = "1",
-	[TWO] = "12",
-	[THREE] = "123",
-	[FOUR] = "1234",
-	[FIVE] = "  12345",
-	[SIX] = "  12345",
-	[SEVEN] = "123 456",
-	[EIGHT] = "A2143244535", 
-	[NINE] = "2143244535",
-	[TEN] = "",
-	[ELEVEN] = "asbj", 
-	[TWELVE] = "", 
-	[THIRTEEN] = "       ",
-	[FOURTEEN] = "-00123", 
-	[FIVETEEN] = "--00123", 
-	[SIXTEEN] = "-+-+-00123",
-	[SEVENTEEN] = "-+-+-00123",
-	[EIGHTEEN] = "++00123",
-	[NINETEEN] = "++--00123",
-	[TWENTY] = "  -00123",
-	[TWENTYONE] = "		--00123", 
-	[TWENTYTWO] = "	 	 ---00123", 
-	[DONE] = "",
+static const char *const	g_tests[] = {
+[ZERO] = "",
+[ONE] = "1",
+[TWO] = "12",
+[THREE] = "123",
+[FOUR] = "1234",
+[FIVE] = "  12345",
+[SIX] = "  12345",
+[SEVEN] = "123 456",
+[EIGHT] = "A2143244535",
+[NINE] = "2143244535",
+[TEN] = "",
+[ELEVEN] = "asbj",
+[TWELVE] = "",
+[THIRTEEN] = "       ",
+[FOURTEEN] = "-00123",
+[FIVETEEN] = "--00123",
+[SIXTEEN] = "-+-+-00123",
+[SEVENTEEN] = "-+-+-00123",
+[EIGHTEEN] = "++00123",
+[NINETEEN] = "++--00123",
+[TWENTY] = "  -00123",
+[TWENTYONE] = "		--00123",
+[TWENTYTWO] = "	 	 ---00123",
 };
 
 int	atoi_cmp(int test_count)
@@ -46,22 +45,22 @@ int	atoi_cmp(int test_count)
 	int		result_org;
 	int		result_ft;
 
-	result_org = atoi(tests[test_count]);
-	result_ft = ft_atoi(tests[test_count]);
+	result_org = atoi(g_tests[test_count]);
+	result_ft = ft_atoi(g_tests[test_count]);
 	if (result_org != result_ft)
 	{
 		g_fail_atoi += ft_log_int(test_count, result_org, result_ft);
-		dprintf(2, "tcase: %s\n", tests[test_count]);
+		dprintf(2, "tcase: %s\n", g_tests[test_count]);
 		g_fail_atoi = 1;
 	}
-	else 
+	else
 		g_fail_atoi = 0;
 	return (g_fail_atoi);
 }
 
 int	atoi_test(int test_count)
 {
-	if (test_count == DONE)
+	if (test_count == sizeof(g_tests) / sizeof(g_tests[0]))
 		return (FINISH);
 	atoi_cmp(test_count);
 	return (g_fail_atoi);
