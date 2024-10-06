4eeb6470607282eebe2d141e28097cd2444a7590
sebastianpenning <whimizera@mailbox.org>
2024-10-06 14:33:02 +0200

    segfault test atoi

--- tests/libft_tests/tests/ft_atoi_test.c
+++ tests/libft_tests/tests/ft_atoi_test.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/21 23:03:23 by spenning      #+#    #+#                 */
-/*   Updated: 2024/08/29 17:49:26 by crasche       ########   odam.nl         */
+/*   Updated: 2024/10/06 14:32:13 by mynodeus      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,22 +40,38 @@ static const char *const	g_tests[] = {
 [TWENTYTWO] = "	 	 ---00123",
 };
 
+void	atoi_fork(int test_count, pid_t *child, int (*f)(const char *))
+{
+	*child = fork();
+	if (*child == -1)
+		exit(1);
+	if (*child == 0)
+	{
+		f(g_tests[test_count]);
+		exit(0);
+	}
+}
+// this test does not have memory test because it was too difficult to distinguish between
+// normal memory issue compared with normal atoi versus abnormal mem issue
 int	atoi_cmp(int test_count)
 {
-	int		result_org;
-	int		result_ft;
+	pid_t	childs[2];
+	int		ft;
+	int		org;
 
-	result_org = atoi(g_tests[test_count]);
-	result_ft = ft_atoi(g_tests[test_count]);
-	if (result_org != result_ft)
+	atoi_fork(test_count, &childs[0], &atoi);
+	atoi_fork(test_count, &childs[1], &ft_atoi);
+	if (wait_child(childs[0]) != wait_child(childs[1]))
+		return (printf(RED " SEGFAULT "RESET));
+	ft_atoi(g_tests[test_count]); 
+	ft = ft_atoi(g_tests[test_count]);
+	org = atoi(g_tests[test_count]);
+	if (ft != org)
 	{
-		g_fail_atoi += ft_log_int(test_count, result_org, result_ft);
-		dprintf(2, "tcase: %s\n", g_tests[test_count]);
-		g_fail_atoi = 1;
+		g_fail_atoi += ft_log_int(test_count, org, ft);
+		dprintf(2, "tcase: [1] %s\n", g_tests[test_count]);
 	}
-	else
-		g_fail_atoi = 0;
-	return (g_fail_atoi);
+	return (0);
 }
 
 int	atoi_test(int test_count)
