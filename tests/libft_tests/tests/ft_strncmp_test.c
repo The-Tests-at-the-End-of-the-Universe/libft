51f3d20615e602a90819f0eaad35803ad4d8e794
sebastianpenning <whimizera@mailbox.org>
2024-10-14 10:36:47 +0200

    strncmp fail tests added

--- tests/libft_tests/tests/ft_strncmp_test.c
+++ tests/libft_tests/tests/ft_strncmp_test.c
@@ -6,7 +6,7 @@
 /*   By: mynodeus <mynodeus@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/17 06:27:38 by mynodeus      #+#    #+#                 */
-/*   Updated: 2024/10/14 09:03:20 by mynodeus      ########   odam.nl         */
+/*   Updated: 2024/10/14 10:35:54 by mynodeus      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,19 @@ static t_strncmp_test	g_tests[] = {
 [3] = {"dfsfdsf?", "??cbdscds", 6},
 [4] = {"", "", 0},
 [5] = {" ", " ", 2},
+[6] = {"aaaaaa", "bbbbb", 0},
+[7] = {"aaaaaa", "", 5},
+[8] = {"aaaaaaaaaa", "bbbbbb", 3},
+[9] = {"", "abbbbbb", 5},
+};
+
+static t_strncmp_test	g_ftests[] = {
+[0] = {"", "", 0}, 
+[1] = {"aaaaaa", NULL, 5},
+[2] = {NULL, "bbbbbb", 5},
+[3] = {"a", "bbbbbb", 5},
+[4] = {"aaaaaaa", "abbbbbb", 100},
+[5] = {"aaaaaaa", "abbbbbb", -100},
 };
 
 void	strncmp_fork(int test_count, pid_t *child, \
@@ -67,10 +80,25 @@ int	strncmp_cmp(int test_count)
 	return (0);
 }
 
-int	strncmp_test(int test_count)
+int	strncmp_test(int test_count, char *fail_flag)
 {
+	size_t	test;
+
+	if (test_count == 9)
+		g_tests[test_count].test = g_tests[test_count].test2 + 1; 
 	if (test_count == sizeof(g_tests) / sizeof(g_tests[0]))
 		return (FINISH);
+	if (fail_flag)
+	{
+		if (test_count == sizeof(g_ftests) / sizeof(g_ftests[0]))
+			return (FINISH);	
+		if (!strcmp("-ft", fail_flag))
+			test = ft_strncmp(g_ftests[test_count].test, g_ftests[test_count].test2, g_ftests[test_count].n);
+		if (!strcmp("-og", fail_flag))
+			test = strncmp(g_ftests[test_count].test, g_ftests[test_count].test2, g_ftests[test_count].n);
+		(void)test;
+		return (g_fail_strncmp);
+	}
 	strncmp_cmp(test_count);
 	return (g_fail_strncmp);
 }
